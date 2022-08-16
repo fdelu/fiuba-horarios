4ccd520acbf127fac55205093b1278463f31e1cf
fdelu <fdeluca@fi.uba.ar>
2022-08-15 22:46:58 -0300

    Fix bug con combinaciones con materias forzadas

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 import json
 import os
-from itertools import combinations, product, starmap
+from itertools import combinations, product
 from concurrent.futures import ThreadPoolExecutor, wait
 from threading import Lock
 
@@ -53,7 +53,7 @@ def _horas_overlap(horas_1, horas_2):
     return max(horas_1["inicio"], horas_2["inicio"]) < min(horas_1["fin"], horas_2["fin"])
 
 
-def _cursos_overlap(cursos):
+def _comprobar_cursos(cursos):
     for curso_1, curso_2 in combinations(cursos, 2):
         for dia in set(curso_1["horarios"].keys()).intersection(set(curso_2["horarios"].keys())):
             for horas_1, horas_2 in product(curso_1["horarios"][dia], curso_2["horarios"][dia]):
@@ -64,20 +64,16 @@ def _cursos_overlap(cursos):
 
 def _comprobar_materias(materias, lock, posibles):
     for cursos in product(*map(lambda x: CURSOS[x], materias)):
-        if not _cursos_overlap(cursos):
+        if not _comprobar_cursos(cursos):
             lock.acquire()
             posibles.append(zip(materias, cursos))
             lock.release()
 
 
 def get_combinaciones_posibles(opciones, forzadas, cantidad):
-    print(opciones)
     resto = set(opciones).difference(set(forzadas))
     comb_resto = combinations(resto, cantidad - len(forzadas))
-    if forzadas:
-        total = starmap(lambda x, y: (*x, *y), product(forzadas, comb_resto))
-    else:
-        total = comb_resto
+    total = map(lambda x: (*forzadas, *x), comb_resto)
     posibles = []
     lock = Lock()
     with ThreadPoolExecutor() as threadpool:
@@ -87,4 +83,5 @@ def get_combinaciones_posibles(opciones, forzadas, cantidad):
                 _comprobar_materias, materias, lock, posibles))
         wait(futures)
 
+    print(posibles)
     return posibles
